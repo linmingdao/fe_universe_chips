f648c26c2212afdaada66a0e5cab21ec04669677
Linmingdao <mr.lmd@outlook.com>
2023-02-08 11:57:00 +0800

    完成时间切片的改造

--- React/case_05_fiber/index_timeslice.js
+++ React/case_05_fiber/index_timeslice.js
@@ -8,17 +8,36 @@ Fiber执行阶段
     协调阶段：Reconcilation可以认为是diff阶段，该阶段可被中断，会造成节点变更，如新增、删除等，这些变更react称为副作用effect
     提交阶段：将上一个阶段计算出来的所有需要处理的副作用一次性执行。这个阶段必须同步执行，不能被打断
  **/
-let rootFiber = require('./elements');
 
+// Fiber mock data
+let A1 = { type: 'div', key: 'A1' };
+let B1 = { type: 'div', key: 'B1', return: A1 };
+let B2 = { type: 'div', key: 'B2', return: A1 };
+let C1 = { type: 'div', key: 'C1', return: B1 };
+let C2 = { type: 'div', key: 'C2', return: B1 };
+A1.child = B1;
+B1.sibling = B2;
+B1.child = C1;
+C1.sibling = C2;
+
+// Fiber root
+let rootFiber = A1;
 let nextUnitOfWork; // 下一个执行单元
 
-function workLoop() {
-  while (nextUnitOfWork) {
+function workLoop(deadline) {
+  // 如果有空闲时间 或者任务已经超时，并且还有下一个待执行的工作单元，则执行任务
+  while (
+    (deadline.timeRemaining() > 0 || deadline.didTimeout) &&
+    nextUnitOfWork
+  ) {
     // 如果有待执行的执行单元，就执行，performUnitOfWork会返回下一个执行单元
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
   }
 
-  if (!nextUnitOfWork) {
+  // 如果没有剩余时间，需要放弃执行任务的控制权，将控制权交给浏览器（等待浏览器下一次空闲时间继续执行任务）
+  if (nextUnitOfWork) {
+    requestIdleCallback(workLoop, { timeout: 1000 });
+  } else {
     console.log('render阶段结束');
   }
 }
@@ -53,4 +72,4 @@ function beginWork(fiber) {
 
 nextUnitOfWork = rootFiber;
 
-workLoop();
+requestIdleCallback(workLoop, { timeout: 1000 });
