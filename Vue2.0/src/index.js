bfd1c536b7f1c592a055f3918f6ca550b2b98733
Linmingdao <mr.lmd@outlook.com>
2023-03-02 10:50:14 +0800

    init流程之数据的劫持

--- Vue2.0/src/index.js
+++ Vue2.0/src/index.js
@@ -1,6 +1,13 @@
 // vue入口文件
+import initMixin from './init';
 
-export default {
-  a: 1,
-  b: 2,
-};
+// 思考：为何vue不采用es6的类的写法？
+// 因为类的推荐写法是所有的拓展都在类的内部进行，不推荐通过 Vue.prototype.a = xx 这样形式可以分文件
+
+function Vue(options) {
+  this._init(options);
+}
+
+initMixin(Vue);
+
+export default Vue;
