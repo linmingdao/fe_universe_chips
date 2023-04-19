783ef71abebb628435f34b93047f9feab223bd20
Linmingdao <mr.lmd@outlook.com>
2023-04-19 19:47:16 +0800

    feat: P03_reactive

---
+++ Vue3.0/P04_effect/packages/reactivity/src/baseHandler.ts
@@ -0,0 +1,13 @@
+export const enum ReactiveFlags {
+  IS_REACTIVE = '__v_isReactive',
+}
+
+export const mutableHandler = {
+  get(target: Object, key: string, receiver: ProxyConstructor) {
+    if (key === ReactiveFlags.IS_REACTIVE) return true;
+    return Reflect.get(target, key, receiver);
+  },
+  set(target: Object, key: string, value: any, receiver: ProxyConstructor) {
+    return Reflect.set(target, key, value, receiver);
+  },
+};
