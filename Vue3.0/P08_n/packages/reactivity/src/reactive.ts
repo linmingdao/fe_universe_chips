fc8c693a6811420343a0317aeaa396890b6c13bd
Linmingdao <mr.lmd@outlook.com>
2023-04-23 15:32:02 +0800

    feat: P07_notify_fix_MaximumCallStack解决effect无限循环问题

---
+++ Vue3.0/P08_n/packages/reactivity/src/reactive.ts
@@ -0,0 +1,23 @@
+import { isObject } from '@vue/shared';
+import { ReactiveFlags, mutableHandler } from './baseHandler';
+
+const reactiveMap = new WeakMap(); // key只能是对象
+
+// 1、将数据转换成响应式，只能做对象的代理
+// 2、实现同一个对象，代理多次，返回同一个代理
+// 3、代理对象再次被代理，可以直接返回
+export function reactive(target: any) {
+  if (!isObject(target)) return;
+
+  if (target[ReactiveFlags.IS_REACTIVE]) return target;
+
+  const exisitingProxy = reactiveMap.get(target);
+  if (exisitingProxy) return exisitingProxy;
+
+  // 没有重新定义属性，只是代理
+  const proxy = new Proxy(target, mutableHandler);
+
+  reactiveMap.set(target, proxy);
+
+  return proxy;
+}
