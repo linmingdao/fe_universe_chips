7e76e8208765bb0c2fd25f761f0ac30e12f17baf
Linmingdao <mr.lmd@outlook.com>
2023-04-24 16:16:55 +0800

    doc: 必要注释

--- Vue3.0/P04_effect_reactive_interrelated/packages/reactivity/src/effect.ts
+++ Vue3.0/P04_effect_reactive_interrelated/packages/reactivity/src/effect.ts
@@ -15,9 +15,9 @@ class ReactiveEffect {
 
     try {
       debugger;
-      // 这里需要依赖收集，核心就是将当前的 effect 和 稍后渲染的属性关联在一起
+      // 利用单线程，这里需要依赖收集，核心就是将当前的 effect 和 稍后渲染的属性【互相关联】在一起
       activeEffect = this;
-      // 当稍后调用取值操作的时候，就可以获取到这个全局的 activeEffect
+      // fn调用取值操作的时候（触发proxy的get、set），就可以获取到这个全局的 activeEffect
       return this.fn();
     } finally {
       activeEffect = undefined;
@@ -25,13 +25,12 @@ class ReactiveEffect {
   }
 }
 
-// 1、fn可以根据状态变化重新执行
+// 1、用户指定的fn可以根据状态变化重新执行
 // 2、effect 跟 react useEffect 不一样的是，他会自动进行依赖收集（更加智能的依赖收集），而useEffect需要手动提供依赖（更加精细的依赖收集）
 // 3、effect可以嵌套写
 export function effect(fn: Function) {
   // 创建响应式的effect
   const _effect = new ReactiveEffect(fn);
-
   // 默认先执行一次
   _effect.run();
 }
